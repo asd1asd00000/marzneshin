4daf63e628e05ef78fa6cb9ba1ef40acea9bb610
asd1asd00000 <126613068+asd1asd00000@users.noreply.github.com>
2025-04-20 02:52:11 +0330

    Update and rename maincommand1.sh to maincommand.sh

--- maincommand.sh
+++ maincommand.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Clear the screen
+clear
+
 # Array of scripts with their display names
 scripts=(
     "sudo bash -c \"\$(curl -sL https://raw.githubusercontent.com/asd1asd00000/marzneshin/refs/heads/main/nasbe-marznode.sh)\""
@@ -10,6 +13,7 @@ scripts=(
     "sudo bash -c \"\$(curl -sL https://raw.githubusercontent.com/asd1asd00000/marzneshin/refs/heads/main/backup.to.gmail.sh)\""
     "sudo bash -c \"\$(curl -sL https://raw.githubusercontent.com/asd1asd00000/marzneshin/refs/heads/main/speedtest.sh)\""
     "sudo bash -c \"\$(curl -sL https://github.com/asd1asd00000/marzneshin/raw/refs/heads/main/backup.folder.to.url.sh)\""
+    "bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)"
 )
 
 # Array of display names for scripts
@@ -22,16 +26,21 @@ names=(
     "backup.to.gmail"
     "speedtest"
     "backup.folder.to.url"
+    "install mhsanaei 3x-ui"
 )
+
+# ANSI color code for blue background
+BLUE_BG="\033[44m"
+RESET="\033[0m"
 
 # Display list of scripts
 echo "List of scripts:"
 echo
-echo "marznode:"
+echo -e "${BLUE_BG}marznode:${RESET}"
 echo "1- ${names[0]}"
 echo "2- ${names[1]}"
 echo
-echo "marzneshin:"
+echo -e "${BLUE_BG}marzneshin:${RESET}"
 echo "3- ${names[2]}"
 echo "4- ${names[3]}"
 echo "5- ${names[4]}"
@@ -39,13 +48,14 @@ echo
 echo "6- ${names[5]}"
 echo "7- ${names[6]}"
 echo "8- ${names[7]}"
+echo "9- ${names[8]}"
 echo
 
 # Get user input
 read -p "Enter the script number (1 to ${#scripts[@]}): " choice
 
 # Validate input
-if [[ ! "$choice" =~ ^[1-8]$ ]]; then
+if [[ ! "$choice" =~ ^[1-9]$ ]]; then
     echo "Invalid input! Please enter a number between 1 and ${#scripts[@]}."
     exit 1
 fi
